a9561100fe284e1d625dbae17eaad05c3f009741
amannitt <amn9955@gmail.com>
2017-06-04 02:58:41 +0530

    Added note-maker v1.0

---
+++ note2.js
@@ -0,0 +1,46 @@
+function addnote(){
+	var text = document.getElementById("sticky-text").value;
+
+var para = document.createElement("P");
+para.setAttribute("id","para");
+document.getElementById("txt").appendChild(para);
+document.getElementById("para").innerHTML = text;
+
+
+
+var del = document.createElement("BUTTON");
+del.setAttribute("id","delnote");
+del.setAttribute("onclick","rm()");
+var x = document.createTextNode("X");
+del.appendChild(x);
+
+var edit = document.createElement("BUTTON");
+edit.setAttribute("id","editnote");
+edit.setAttribute("onclick","edit()");
+var e = document.createTextNode("Edit");
+edit.appendChild(e);
+
+var btns = document.createElement("DIV");
+btns.setAttribute("id","btns");
+btns.appendChild(edit);
+btns.appendChild(del);
+
+
+document.body.appendChild(btns);
+document.body.appendChild(para);
+
+}
+function rm(){
+	alert("Are you sure?");
+	document.body.removeChild(para);
+	document.body.removeChild(btns);
+}
+var x = 1;
+function edit(){
+	if(x===1){
+		alert("Type new text in the box and hit Edit again!");
+		x++;
+	}
+	var text2 = document.getElementById("sticky-text").value;
+	document.getElementById("para").innerHTML = text2;
+}
